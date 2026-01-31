ad0e70670c107211dabf8fa4a0c26d20d7d24280
Samrat Crosiya <bandarapublic7@gmail.com>
2026-01-31 23:09:50 +0530

    Initial commit

---
+++ worker.js
@@ -0,0 +1,67 @@
+import { Worker } from 'bullmq';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { QdrantVectorStore } from '@langchain/qdrant';
+import { Document } from '@langchain/core/documents';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { CharacterTextSplitter } from '@langchain/textsplitters';
+import 'dotenv/config';
+
+
+
+const worker = new Worker(
+  'file-upload-queue',
+  async (job) => {
+    console.log(`Job:`, job.data);
+    const data = JSON.parse(job.data);
+    /*
+    Path: data.path
+    read the pdf from path,
+    chunk the pdf,
+    call the openai embedding model for every chunk,
+    store the chunk in qdrant db
+    */
+
+    // Load the PDF
+    const loader = new PDFLoader(data.path);
+    const docs = await loader.load();
+
+
+   // Optional: split into smaller chunks before embedding
+    const splitter = new CharacterTextSplitter({
+      chunkSize: 1000,
+      chunkOverlap: 200,
+       });
+    const splitDocs = await splitter.splitDocuments(docs);
+
+
+
+
+    // const embeddings = new OpenAIEmbeddings({
+    //   model: 'text-embedding-3-small',
+    //   apiKey: '',
+    // });
+
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+      model: 'embedding-001',
+      apiKey: process.env.GOOGLE_API_KEY
+    });
+
+    const vectorStore = await QdrantVectorStore.fromExistingCollection(
+      embeddings,
+      {
+        url: 'http://localhost:6333',
+        collectionName: 'langchainjs-testing',
+      }
+    );
+    await vectorStore.addDocuments(docs);
+    console.log(`All docs are added to vector store`);
+  },
+  {
+    concurrency: 100,
+    connection: {
+      host: 'localhost',
+      port: '6379',
+    },
+  }
+);
